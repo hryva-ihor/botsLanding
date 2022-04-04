c88ac250cf25cac7f5eb3c09f5defa4b507c6b54
hryva-ihor <griva.igor422@gmail.com>
2022-04-04 15:49:20 +0300

    fistcom

---
+++ js/sliders.js
@@ -0,0 +1,185 @@
+$('.product-img-1').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 300,
+    infinite: true,
+    swipe: false,
+    variableWidth: false,
+    fade: true,
+    /*asNavFor: '.product1__slider-small',*/
+    arrows: true,
+    appendArrows: $('.product-img-1'),
+    prevArrow: '<div class="product-btn product__prev">&#8592;</div>',
+    nextArrow: '<div class="product-btn product__next">&#8594;</div>',
+    responsive: [{
+        breakpoint: 701,
+        settings: {
+            slidesToShow: 1,
+            appendArrows: $('.product-img-1'),
+            prevArrow: '<div class="product-btn product__prev">&#8592;</div>',
+            nextArrow: '<div class="product-btn product__next">&#8594;</div>'
+        }
+    }]
+});
+$('.product-img-2').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 300,
+    infinite: true,
+    swipe: false,
+    variableWidth: false,
+    fade: true,
+    /*asNavFor: '.product1__slider-small',*/
+    arrows: true,
+    appendArrows: $('.product-img-2'),
+    prevArrow: '<div class="product-btn product__prev">&#8592;</div>',
+    nextArrow: '<div class="product-btn product__next">&#8594;</div>',
+    responsive: [{
+        breakpoint: 701,
+        settings: {
+            slidesToShow: 1,
+            appendArrows: $('.product-img-2'),
+            prevArrow: '<div class="product-btn product__prev">&#8592;</div>',
+            nextArrow: '<div class="product-btn product__next">&#8594;</div>'
+        }
+    }]
+});
+$('.product-img-3').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 300,
+    infinite: true,
+    swipe: false,
+    variableWidth: false,
+    fade: true,
+    /*asNavFor: '.product1__slider-small',*/
+    arrows: true,
+    appendArrows: $('.product-img-3'),
+    prevArrow: '<div class="product-btn product__prev">&#8592;</div>',
+    nextArrow: '<div class="product-btn product__next">&#8594;</div>',
+    responsive: [{
+        breakpoint: 701,
+        settings: {
+            slidesToShow: 1,
+            appendArrows: $('.product-img-3'),
+            prevArrow: '<div class="product-btn product__prev">&#8592;</div>',
+            nextArrow: '<div class="product-btn product__next">&#8594;</div>'
+        }
+    }]
+});
+
+$('.product-img-w1').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 300,
+    infinite: true,
+    swipe: false,
+    variableWidth: false,
+    fade: true,
+    /*asNavFor: '.product1__slider-small',*/
+    arrows: true,
+    appendArrows: $('.product-img-w1'),
+    prevArrow: '<div class="product-btn product__prev">&#8592;</div>',
+    nextArrow: '<div class="product-btn product__next">&#8594;</div>',
+    responsive: [{
+        breakpoint: 701,
+        settings: {
+            slidesToShow: 1,
+            appendArrows: $('.product-img-w1'),
+            prevArrow: '<div class="product-btn product__prev">&#8592;</div>',
+            nextArrow: '<div class="product-btn product__next">&#8594;</div>'
+        }
+    }]
+});
+$('.product-img-w2').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 300,
+    infinite: true,
+    swipe: false,
+    variableWidth: false,
+    fade: true,
+    /*asNavFor: '.product1__slider-small',*/
+    arrows: true,
+    appendArrows: $('.product-img-w2'),
+    prevArrow: '<div class="product-btn product__prev">&#8592;</div>',
+    nextArrow: '<div class="product-btn product__next">&#8594;</div>',
+    responsive: [{
+        breakpoint: 701,
+        settings: {
+            slidesToShow: 1,
+            appendArrows: $('.product-img-w2'),
+            prevArrow: '<div class="product-btn product__prev">&#8592;</div>',
+            nextArrow: '<div class="product-btn product__next">&#8594;</div>'
+        }
+    }]
+});
+$('.product-img-w3').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 300,
+    infinite: true,
+    swipe: false,
+    variableWidth: false,
+    fade: true,
+    /*asNavFor: '.product1__slider-small',*/
+    arrows: true,
+    appendArrows: $('.product-img-w3'),
+    prevArrow: '<div class="product-btn product__prev">&#8592;</div>',
+    nextArrow: '<div class="product-btn product__next">&#8594;</div>',
+    responsive: [{
+        breakpoint: 701,
+        settings: {
+            slidesToShow: 1,
+            appendArrows: $('.product-img-w3'),
+            prevArrow: '<div class="product-btn product__prev">&#8592;</div>',
+            nextArrow: '<div class="product-btn product__next">&#8594;</div>'
+        }
+    }]
+});
+$('.product-img-wc1').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 300,
+    infinite: true,
+    swipe: false,
+    variableWidth: false,
+    fade: true,
+    /*asNavFor: '.product1__slider-small',*/
+    arrows: true,
+    appendArrows: $('.product-img-wc1'),
+    prevArrow: '<div class="product-btn product__prev">&#8592;</div>',
+    nextArrow: '<div class="product-btn product__next">&#8594;</div>',
+    responsive: [{
+        breakpoint: 701,
+        settings: {
+            slidesToShow: 1,
+            appendArrows: $('.product-img-wc1'),
+            prevArrow: '<div class="product-btn product__prev">&#8592;</div>',
+            nextArrow: '<div class="product-btn product__next">&#8594;</div>'
+        }
+    }]
+});
+$('.product-img-mc1').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 300,
+    infinite: true,
+    swipe: false,
+    variableWidth: false,
+    fade: true,
+    /*asNavFor: '.product1__slider-small',*/
+    arrows: true,
+    appendArrows: $('.product-img-mc1'),
+    prevArrow: '<div class="product-btn product__prev">&#8592;</div>',
+    nextArrow: '<div class="product-btn product__next">&#8594;</div>',
+    responsive: [{
+        breakpoint: 701,
+        settings: {
+            slidesToShow: 1,
+            appendArrows: $('.product-img-mc1'),
+            prevArrow: '<div class="product-btn product__prev">&#8592;</div>',
+            nextArrow: '<div class="product-btn product__next">&#8594;</div>'
+        }
+    }]
+});
